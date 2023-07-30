76992febfa6969a28712236522c7d39564ab2e26
Oleksandr Dimbrovskyi <sanya.dimbrovskyi@gmail.com>
2023-07-30 13:26:43 -0500

    Added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Content from './Components/Content/Content';
 
 function App() {
   return (
     <div className="App">
       <div className='wrapper'>
         <Header />
+        <Content />
+        <Footer />
       </div>
     </div>
   );
---
+++ frontend/src/Components/Footer/Links/Links.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import css from './Links.module.css'
+
+const Links = () => {
+    return(
+        <div className={css.footer_links}>
+              <a href="#link" className={css.link}>Home</a>
+              <a href="#intro" className={css.link}>Features</a>
+              <a href="#link" className={css.link}>Contact</a>
+              <a href="#link" className={css.link}>Privacy Policy</a>
+              <a href="#link" className={css.link}>Twitter</a>
+        </div>
+    )
+}
+
+export default Links
